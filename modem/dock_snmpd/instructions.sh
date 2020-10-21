927ae8bab9ac3b9eb743cd98d02bc4a1a9f095e6
YuvalToker <yuval@gmail.com>
2020-10-21 14:50:14 +0300

    changes in modem, modem.py is incomplete but added

--- modem/dock_snmpd/instructions.sh
+++ modem/dock_snmpd/instructions.sh
@@ -6,6 +6,9 @@ echo "master agentx" >> /etc/snmp/snmpd.conf
 
 snmpd -f -Lf snmpd_file.log -C -M /usr/share/snmp/mibs --rwcommunity=public --master=agentx --agentXSocket=tcp:localhost:161 udp:1662 &
 
+cd create_snmpd
+make example-daemon
+./example-daemon -f -Lf subagent_fil.log -x tcp:localhost:161 &
 #make example-daemon
 #./example-daemon -f -Lf subagent_file.log -x  tcp:localhost:161 &
 
